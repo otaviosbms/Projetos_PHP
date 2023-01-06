10c5476bf31cef8b9c53a3d89cdc561f918a9d6b
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-01-06 04:00:17 -0300

    Add files via upload

--- jokenpo-php.php
+++ jokenpo-php.php
@@ -6,18 +6,20 @@ echo'....................'.PHP_EOL;
 $jogador = readline('Escolha: pedra, papel ou tesoura: ');
 
 
-/*if($jogador == 'pedra' || $jogador == 'papel' || $jogador == 'tesoura'){
-    echo "O jogador escolheu $jogador.".PHP_EOL;
+if($jogador == 'pedra' or $jogador == 'papel' or $jogador == 'tesoura'){
+    echo "voce escolheu $jogador".PHP_EOL;
 }
 
-if($jogador != 'pedra' || $jogador != 'papel' || $jogador != 'tesoura'){
-    echo 'ERRO. Escola uma opção válida.' .PHP_EOL;
-    }*/
+
 
 while($jogador != 'pedra' or $jogador != 'papel' or $jogador != 'tesoura'){
-     $jogador = readline('ERRO. Escolha uma opção válida: ').PHP_EOL;
+    $jogador = readline('ERRO. Escolha uma opção válida: ');
 
-
-     if($jogador == 'pedra' or $jogador == 'papel' or $jogador == 'tesoura')
-        echo "voce escolheu $jogador";
+    if($jogador == 'pedra' or $jogador == 'papel' or $jogador == 'tesoura'){
+        echo "voce escolheu $jogador".PHP_EOL;
+        break;
     }
+
+}
+
+
