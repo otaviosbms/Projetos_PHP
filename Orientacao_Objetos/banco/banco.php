f6d5a8712020c2407d116348fbb597997ac2a672
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-03-20 18:51:38 -0300

    Add files via upload

--- Orientacao_Objetos/banco/banco.php
+++ Orientacao_Objetos/banco/banco.php
@@ -2,9 +2,11 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
+$primeiraConta = new Conta('123.456.789-10', 'Vinicius Dias');
 
-$primeiraConta->depositar(1000);
-$primeiraConta->sacar(300); //isso é ok
+$primeiraConta->depositar(500);
+$primeiraConta->sacar(300);
 
-var_dump($primeiraConta);
+echo $primeiraConta->recuperarNomeTitular().PHP_EOL;
+echo $primeiraConta->recuperarCpfTitular().PHP_EOL;
+echo $primeiraConta->recuperarSaldo().PHP_EOL;
